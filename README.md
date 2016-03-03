e587cae0dc57c203ab2c4113b9c0d155e09ba363
jmontei8 <jmontei8@students.d125.org>
2016-03-03 10:32:39 -0600

    test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CSTEST
+These are my first changes for testing git
---
+++ Throwing.java
@@ -0,0 +1,17 @@
+
+//********************************************************************
+//  Throwing.java       Author: Lewis/Loftus/Cocking
+//
+//  Demonstrates throwing an exception.
+//********************************************************************
+public class Throwing
+{
+   //-----------------------------------------------------------------
+   //  Throws an exception.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+      throw new IllegalStateException("just testing!");
+   }
+}
+
